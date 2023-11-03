67747c19cd460919ceced364ea343f97b035fd6d
zhenia.podobreev <zhenia.podobreev@cruxlab.com>
2023-11-03 17:10:38 +0200

    added show password icons

--- src/components/atoms/inputWithLabel/InputWithLabel.tsx
+++ src/components/atoms/inputWithLabel/InputWithLabel.tsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import './inputWithLabel.scss'
+import openEyeIcon from '../../../assets/open-eye.png'
+import hideEyeIcon from '../../../assets/hide-eye.png'
 
 interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
   textLabel: string
@@ -7,13 +9,22 @@ interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
 
 export const InputWithLabel = (pros: Props) => {
   const {type, id, value, onChange, textLabel } = pros
+  const [isPassVisible, setIsPassVisible] = useState(false)
 
   return (
     <div className="input-container">
-      <input className="input-container__input" required type={type} id={id} value={value} onChange={onChange}/>
+      <input className="input-container__input" required type={isPassVisible ? 'text' : type} id={id} value={value} onChange={onChange}/>
       <label className="input-container__label" htmlFor={id} >
         {textLabel}
       </label>
+      {type === 'password' && (
+        <img
+          className="input-container__icon"
+          src={!isPassVisible ? openEyeIcon : hideEyeIcon}
+          alt="plate display icons" 
+          onClick={() => setIsPassVisible(!isPassVisible)} 
+        />
+      )}
     </div>
   )
 }
