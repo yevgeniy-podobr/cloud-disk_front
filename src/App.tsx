7f7526ba579233e0dbccf4da845aa9fc4c592fa4
zhenia.podobreev <zhenia.podobreev@cruxlab.com>
2023-11-21 18:30:35 +0200

    -removed unusing import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import { BrowserRouter, Route, Routes, Navigate, HashRouter } from 'react-router-dom';
+import { Route, Routes, Navigate, HashRouter } from 'react-router-dom';
 import './App.scss';
 import { Navbar } from './components';
 import { Authorization } from './components/molecules';
