25a2ddf1d8da217df9355ee5bac5525db9dfb0d1
zhenia.podobreev <zhenia.podobreev@cruxlab.com>
2023-11-28 18:06:43 +0200

    added meta tag

--- src/utils/constants/fileConstants.ts
+++ src/utils/constants/fileConstants.ts
@@ -6,29 +6,3 @@ export enum EFolderDisplayOptions {
 export enum EFileType {
   dir = 'dir'
 }
-
-export enum TypeResume {
-  png = 'image/png',
-  jpg = 'image/jpg',
-  jpeg = 'image/jpeg',
-  gif = 'image/gif',
-  heic = 'image/heic',
-  doc = 'application/msword',
-  pdf = 'application/pdf',
-  docx = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
-}
-
-export enum TypeVideo {
-  webm = 'video/webm',
-  mp4 = 'video/mp4',
-  ogg = 'video/ogg',
-  ogv = 'video/ogv',
-  avi = 'video/avi',
-  mov = 'video/mov',
-  quicktime = 'video/quicktime',
-  m4v = 'video/m4v',
-}
-
-export enum VideoAllTypes {
-  all = 'video/mp4,video/x-m4v,video/*',
-}
