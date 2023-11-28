4854109dce1a4b0c22b9f0b1fefbb53c72fab03a
zhenia.podobreev <zhenia.podobreev@cruxlab.com>
2023-11-28 12:04:37 +0200

    added check for same folder name

--- src/components/molecules/addFolderModal/AddFolderModal.tsx
+++ src/components/molecules/addFolderModal/AddFolderModal.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import './addFolderModal.scss';
 import { Input } from "../../atoms/input";
+import { useTypedSelector } from "../../../redux";
+import { toast } from "react-toastify";
 
 interface IProps {
   setAddFolderModalOpen: React.Dispatch<React.SetStateAction<boolean>>
@@ -10,6 +12,18 @@ interface IProps {
 export const AddFolderModal = (props: IProps) => {
   const { setAddFolderModalOpen, createFolderHandler } = props
   const [folderName, setFolderName] = useState('')
+  const files = useTypedSelector(state => state.file.files)
+
+  const handleCreateFolder = () => {
+    const isFolderWithSameName = files.some(file => file.name === folderName)
+    if (isFolderWithSameName) {
+      toast.error('Folder with this name exists')
+    } else {
+      createFolderHandler(folderName)
+      setAddFolderModalOpen(false)
+      setFolderName('')
+    }
+  }
 
   return (
     <div className="add-folder-modal" onClick={() => setAddFolderModalOpen(false)}>
@@ -34,11 +48,7 @@ export const AddFolderModal = (props: IProps) => {
         <div className="add-folder-modal__btn-create-wrapper">
           <button 
             className="add-folder-modal__btn-create button"
-            onClick={() => {
-              createFolderHandler(folderName)
-              setAddFolderModalOpen(false)
-              setFolderName('')
-            }}
+            onClick={handleCreateFolder}
             disabled={!folderName}
           >
             Create
